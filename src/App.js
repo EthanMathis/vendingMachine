e931c9293480ec1b13a484af9f1f632312cb1a43
EthanMathis <ethan.mathis@outlook.com>
2021-08-12 00:34:37 -0500

    minimal styling completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useState } from 'react';
 import { Container, Row, Col, Button, Card, CardTitle, CardBody, CardText, ButtonGroup } from 'reactstrap';
+import '../src/styles/main.css';
 
 
 const App = () => {
@@ -15,60 +16,72 @@ const App = () => {
   const handleSodaRequest = (e) => {
     let sodaList = [...purchaseList];
     let newCount = sodaCount;
-    if (hasQuarter === true) {
+    if (hasQuarter === true && sodaCount > 0) {
       sodaList.push(e.target.innerText)
+      newCount--;
+      setMessage(`You recieve a ${e.target.innerText}`)
+      setPurchaseList(sodaList)
+      setHasQuarter(false)
+      setSodaCount(newCount)
+    } else if (sodaCount <= 0) {
+      setMessage("Sorry :( We're all sold out...")
+    } else if (hasQuarter === false) {
+      setMessage("You didn't give me any money.... How did you think this worked?")
     }
-    setPurchaseList(sodaList)
-    setHasQuarter(false)
-    newCount--;
-    setSodaCount(newCount)
-    console.log(sodaCount)
   }
 
 
 
 
   return (
-    <Container className="border rounded border-primary w-75">
+    <Container className="border rounded border-primary w-75 mt-4" id="main-container">
+      <Row>
+        <div className="col-xs-2 d-flex justify-content-between">
+          <h1 className="m-2">Vending Machine</h1>
+          <ButtonGroup vertical size="sm" className="m-2">
+            <Button className="btn-info mt-2" onClick={() => setHasQuarter(true)}>Insert Coin</Button>
+            <Button className="btn-danger" onClick={() => setHasQuarter(false)}>Return Coin</Button>
+          </ButtonGroup>
+        </div>
+        <div className="col-xs-10">
+          <ButtonGroup className="d-flex">
+            <Button onClick={handleSodaRequest} className="col-12 btn-success m-2 p-2">Coca-Cola</Button>
+            <Button onClick={handleSodaRequest} className="col-12 btn-success m-2 p-2">Diet Coke</Button>
+          </ButtonGroup>
 
-      <ButtonGroup className="d-flex">
-        <Button onClick={handleSodaRequest} className="col-12 btn-success m-2 p-2">Coca-Cola</Button>
-        <Button onClick={handleSodaRequest} className="col-12 btn-success m-2 p-2">Diet Coke</Button>
-      </ButtonGroup>
+          <ButtonGroup className="d-flex">
+            <Button onClick={handleSodaRequest} className="col-12 btn-success m-2 p-2">Dr. Pepper</Button>
+            <Button onClick={handleSodaRequest} className="col-12 btn-success m-2 p-2">Sprite</Button>
+          </ButtonGroup>
 
-      <ButtonGroup className="d-flex">
-        <Button onClick={handleSodaRequest} className="col-12 btn-success m-2 p-2">Dr. Pepper</Button>
-        <Button onClick={handleSodaRequest} className="col-12 btn-success m-2 p-2">Sprite</Button>
-      </ButtonGroup>
-
-      <ButtonGroup className="d-flex">
-        <Button onClick={handleSodaRequest} className="col-12 btn-success m-2 p-2">Fanta</Button>
-        <Button onClick={handleSodaRequest} className="col-12 btn-success m-2 p-2">Ginger Ale</Button>
-      </ButtonGroup>
+          <ButtonGroup className="d-flex">
+            <Button onClick={handleSodaRequest} className="col-12 btn-success m-2 p-2">Fanta</Button>
+            <Button onClick={handleSodaRequest} className="col-12 btn-success m-2 p-2">Ginger Ale</Button>
+          </ButtonGroup>
+        </div>
+      </Row>
 
 
 
-      <ButtonGroup className="d-flex m-2">
-        <Button className="btn-info" onClick={() => setHasQuarter(true)}>Insert Coin</Button>
-        <Button className="btn-danger" onClick={() => setHasQuarter(false)}>Return Coin</Button>
-      </ButtonGroup>
 
       <Row>
-        <Col>
-          <Card className="m-2">
-            <CardTitle>Message Center</CardTitle>
-            <CardBody></CardBody>
+        <Col className="mb-2">
+          <Card className="m-2" id="card">
+            <CardTitle className="text-center">Purchase List</CardTitle>
+            <hr />
+            <CardBody>{purchaseList.length === 0 ? <><div>No one has bought anything from me today...</div> <br /> <div>Are my prices too high?</div> </> : purchaseList.map(thing => <div>{thing}</div>)}</CardBody>
           </Card>
         </Col>
         <Col>
-          <Card className="m-2">
-            <CardTitle>Purchase List</CardTitle>
-            <CardBody>{purchaseList.map(thing => <div>{thing}</div>)}</CardBody>
+          <Card className="m-2" id="card">
+            <CardTitle className="text-center">Message Center</CardTitle>
+            <hr />
+            <CardBody>{message} <br /> There are {sodaCount} beverages left.</CardBody>
           </Card>
+          {hasQuarter === false && <Card className="m-2" id="card"><CardText className="text-center m-2">Please insert a quarter into the machine...</CardText></Card>}
         </Col>
 
       </Row>
-      {hasQuarter === false && <Card className="m-2"><CardText className="text-center m-2">Please insert a quarter into the machine...</CardText></Card>}
 
     </Container>
   )
